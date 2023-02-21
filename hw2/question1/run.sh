0413244c0d4d0cf6e2f0701cf36c9f115faa6044
sandesh katta <sandesh.katta97@gmail.com>
2023-02-20 22:24:16 -0500

    final

--- hw2/question1/run.sh
+++ hw2/question1/run.sh
@@ -5,15 +5,25 @@ for ((i=10;i<=128;i=i+1));
 do
     ./a.out 0 $i -1
 done
+echo "Mesh k=0"
+for ((i=3;i<=20;i=i+1)); 
+do
+    ./a.out 1 $i 0
+done
 echo "Mesh k=4"
 for ((i=3;i<=20;i=i+1)); 
 do
     ./a.out 1 $i 4
 done
 echo "Mesh k=16"
-for ((i=3;i<=30;i=i+1)); 
+for ((i=3;i<=20;i=i+1)); 
 do
     ./a.out 1 $i 16
+done
+echo "Tree k=0"
+for ((i=2;i<=10;i=i+1)); 
+do
+    ./a.out 2 $i 0
 done
 echo "Tree k=4"
 for ((i=2;i<=10;i=i+1)); 
--- hw2/question2/run.sh
+++ hw2/question2/run.sh
@@ -1,27 +1,37 @@
 #!/bin/sh
 g++ *.cpp -o ./a.out
 echo "ring"
-for ((i=10;i<=128;i=i+1)); 
+for ((i=8;i<=30;i=i+1)); 
 do
     ./a.out 0 $i -1
 done
+echo "Mesh k=0"
+for ((i=4;i<=10;i=i+2)); 
+do
+    ./a.out 1 $i 0
+done
 echo "Mesh k=4"
-for ((i=3;i<=20;i=i+1)); 
+for ((i=4;i<=10;i=i+2)); 
 do
     ./a.out 1 $i 4
 done
 echo "Mesh k=16"
-for ((i=3;i<=30;i=i+1)); 
+for ((i=4;i<=10;i=i+2)); 
 do
     ./a.out 1 $i 16
 done
+echo "Tree k=0"
+for ((i=2;i<=6;i=i+1)); 
+do
+    ./a.out 2 $i 0
+done
 echo "Tree k=4"
-for ((i=2;i<=10;i=i+1)); 
+for ((i=2;i<=6;i=i+1)); 
 do
     ./a.out 2 $i 4
 done
 echo "Tree k=16"
-for ((i=2;i<=10;i=i+1)); 
+for ((i=2;i<=6;i=i+1)); 
 do
     ./a.out 2 $i 16
 done
