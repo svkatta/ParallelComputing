b54039bfdf3afb67af34dbdbde2c010ed7924d4c
sandesh katta <sandesh.katta97@gmail.com>
2023-03-06 21:24:36 -0500

    final-1

--- hw3/question2/runner.sh
+++ hw3/question2/runner.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-g++ main-sub.cpp -pthread -o ./a.out
-# echo "threads used 4 array size 10000000"
-# for ((i=0;i<10;i = i +1)); 
-# do
-#     ./a.out 4 1000000
-# done
+g++ main.cpp -pthread -o ./a.out
 for j in 1 2 4 8 16 32 64 
 do
     for ((i=0;i<10;i = i +1)); 
--- hw3/question2/runner2.sh
+++ hw3/question2/runner2.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
-g++ main-sub.cpp -pthread -o ./a.out
-for j in 10000 100000 1000000 10000000
+g++ main.cpp -pthread -o ./a.out
+for i in 1 16 32
 do
-    for ((i=0;i<10;i = i +1)); 
+    for j in 10000 100000 1000000 10000000
     do
-        ./a.out 1 $j
-    done
-done
-for j in 10000 100000 1000000 10000000
-do
-    for ((i=0;i<10;i = i +1)); 
-    do
-        ./a.out 16 $j
-    done
-done
-for j in 10000 100000 1000000 10000000
-do
-    for ((i=0;i<10;i = i +1)); 
-    do
-        ./a.out 32 $j
+        ./a.out $i $j
     done
 done
 rm -rf ./a.out 
