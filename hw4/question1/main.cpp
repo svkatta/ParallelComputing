5c96f991187bff9c203b241c8d0a9ba858fa4b3d
sandesh katta <sandesh.katta97@gmail.com>
2023-03-29 21:29:16 -0400

    final commit 1

--- hw4/question1/main.cpp
+++ hw4/question1/main.cpp
@@ -9,33 +9,43 @@ int main(int argc, char** argv){
     MPI_Comm_size(MPI_COMM_WORLD, &cluster_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    int comm_rank = (rank %(cluster_size/2)) + ((rank< cluster_size/2) ?  (cluster_size/2) : 0);
+
 
     int message_size = atoi(argv[1]);
     int n_trails = 1000;
 
-    int message[message_size];
+    char message[message_size];
     float start_time,end_time;
 
     float total_time = 0;
+    MPI_Barrier(MPI_COMM_WORLD); // Synchronize all processes
     for (int i = 0; i < n_trails ; i++){
-        MPI_Barrier(MPI_COMM_WORLD); // Synchronize all processes
-        if (rank ==0){
+        if (rank < (cluster_size/2)){
             start_time = MPI_Wtime();
-            MPI_Send(message, message_size, MPI_INT, 1, 0, MPI_COMM_WORLD); 
-            MPI_Recv(message, message_size, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+            MPI_Send(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD); 
+            MPI_Recv(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
             end_time = MPI_Wtime();
             total_time += end_time - start_time;
-        }else if (rank ==1){
-            MPI_Recv(message, message_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-            MPI_Send(message, message_size, MPI_INT, 0, 0, MPI_COMM_WORLD); 
+        }else{
+            MPI_Recv(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+            MPI_Send(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD); 
         }
-
     }
 
+    // gather all the times
+    float all_times[cluster_size];
+    MPI_Gather(&total_time, 1, MPI_FLOAT, all_times, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
     if(rank ==0){
-        // printf("Average time for %d trails with message size %d is %f\n",n_trails,message_size,(total_time)/(n_trails*2));
-        printf("%f\n",(total_time)/(n_trails*2));
+        //printf("Average time for %d trails with message size %d is %f\n",n_trails,message_size,(total_time)/(n_trails*2));
+        float total_sum=0;
+        for (int i = 0; i < cluster_size; i++){
+            total_sum += all_times[i];
+        }
+        printf("%f \n", (total_sum)/(n_trails*2));
     }
 
     MPI_Finalize();
 }
+
--- hw4/question2/main.cpp
+++ hw4/question2/main.cpp
@@ -9,14 +9,13 @@ int main(int argc, char** argv){
     MPI_Comm_size(MPI_COMM_WORLD, &cluster_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    
-    int comm_rank = (rank %(size_of_cluster/2)) + (rank< size_of_cluster/2) ?  (size_of_cluster/2) : 0;
+    int comm_rank = (rank %(cluster_size/2)) + ((rank< cluster_size/2) ?  (cluster_size/2) : 0);
 
 
     int message_size = atoi(argv[1]);
     int n_trails = 1000;
 
-    int message[message_size];
+    char message[message_size];
     float start_time,end_time;
 
     float total_time = 0;
@@ -24,13 +23,13 @@ int main(int argc, char** argv){
     for (int i = 0; i < n_trails ; i++){
         if (rank < (cluster_size/2)){
             start_time = MPI_Wtime();
-            MPI_Send(message, message_size, MPI_INT, comm_rank, 0, MPI_COMM_WORLD); 
-            MPI_Recv(message, message_size, MPI_INT, comm_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+            MPI_Send(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD); 
+            MPI_Recv(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
             end_time = MPI_Wtime();
             total_time += end_time - start_time;
         }else{
-            MPI_Recv(message, message_size, MPI_INT, comm_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-            MPI_Send(message, message_size, MPI_INT, comm_rank, 0, MPI_COMM_WORLD); 
+            MPI_Recv(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+            MPI_Send(message, message_size, MPI_CHAR, comm_rank, 0, MPI_COMM_WORLD); 
         }
     }
 
@@ -39,8 +38,14 @@ int main(int argc, char** argv){
     MPI_Gather(&total_time, 1, MPI_FLOAT, all_times, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     if(rank ==0){
-        printf("Average time for %d trails with message size %d is %f\n",n_trails,message_size,(total_time)/(n_trails*2));
+        //printf("Average time for %d trails with message size %d is %f\n",n_trails,message_size,(total_time)/(n_trails*2));
+        float total_sum=0;
+        for (int i = 0; i < cluster_size; i++){
+            total_sum += all_times[i];
+        }
+        printf("%f \n", (total_sum)/(cluster_size*n_trails));
     }
 
     MPI_Finalize();
 }
+
